8433c2d671e5422af398ec7c1c7b61ef40334854
PHAN DOMINIQUE p1402841 <dominique.phan@etu.univ-lyon1.fr>
2017-03-02 10:10:22 +0100

    Question 49 fini

---
+++ Checklists/AllListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AllListViewController.swift
+//  Checklists
+//
+//  Created by iem on 02/03/2017.
+//  Copyright © 2017 iem. All rights reserved.
+//
+
+import UIKit
+
+class AllListViewController: UITableViewController {
+    
+    var list = [ChecklistItem]()
+    
+    
+}
---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by iem on 02/03/2017.
+//  Copyright © 2017 iem. All rights reserved.
+//
+
+import UIKit
+
+class ListDetailViewController: UITableViewController {
+    
+}
